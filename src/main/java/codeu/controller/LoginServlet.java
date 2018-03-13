4838f4ee9223302d678182038c97df6b78da0b1a
chenpionaar1998 <chenpionaar1998@gmail.com>
2018-03-12 20:34:04 -0700

    typo fixed, username added for 'TODO' for tracking

--- src/main/java/codeu/controller/LoginServlet.java
+++ src/main/java/codeu/controller/LoginServlet.java
@@ -74,7 +74,7 @@ public class LoginServlet extends HttpServlet {
     }
 
     if (!userStore.isUserRegistered(username)) {
-      // TODO:add apssword
+      // TODO (chenpionaar1998) : add password
       User user = new User(UUID.randomUUID(), username, null, Instant.now());
       userStore.addUser(user);
     }
