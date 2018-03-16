4f30c21e90079ed8baa6393e60c8252c6e5de5f4
sherryyang2016 <sherryyang2016@gmail.com>
2018-03-15 22:33:39 -0700

    Handling the case of illegal input for username

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -16,13 +16,20 @@ public class RegisterServlet extends HttpServlet{
       throws IOException, ServletException {
     request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request,response);
   }
+  
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
 
     String username = request.getParameter("username");
     String password = request.getParameter("password");
-
+    
+    if (!username.matches("[\\w*\\s*]*")) {
+        request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
+        request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+        return;
+    }
+    
     response.getWriter().println("<p>Username: " + username + "</p>");
     response.getWriter().println("<p>Password: " + password + "</p>");
   }
