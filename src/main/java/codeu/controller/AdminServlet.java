c866c06957b4edc0dd7fb9e502e5929f3a4881a4
chenpionaar1998 <chenpionaar1998@gmail.com>
2018-04-03 15:30:55 -0700

    change doGet so it can only be accessed by admin

--- src/main/java/codeu/controller/AdminServlet.java
+++ src/main/java/codeu/controller/AdminServlet.java
@@ -1,6 +1,8 @@
 package codeu.controller;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,10 +14,26 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet class responsible for the admin page. */
 public class AdminServlet extends HttpServlet {
 
+  private static final String[] permittedUser = {"adminTest"};
+
+  private boolean contains(String[] arr, String target){
+    for( String element: arr){
+      if(element.equals(target))return true;
+    }
+    return false;
+  }
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
-    request.getRequestDispatcher("/WEB-INF/view/admin.jsp").forward(request,response);
+        if (contains(permittedUser ,(String) request.getSession().getAttribute("user"))) {
+          request.getRequestDispatcher("/WEB-INF/view/admin.jsp").forward(request,response);
+        } else if (request.getSession().getAttribute("user") == null) {
+          request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request,response);
+        } else {
+          request.setAttribute("error", "This user does not have permission.");
+          request.getRequestDispatcher("/index.jsp").forward(request,response);
+        }
   }
-  
+
 }
