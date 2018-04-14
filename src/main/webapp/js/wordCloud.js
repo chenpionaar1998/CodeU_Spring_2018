412572a3fd20c892271907391203b8fc7cd8705c
chenpionaar1998 <chenpionaar1998@gmail.com>
2018-04-14 02:27:10 -0700

    adding js for creating word cloud and css for formatting the admin page

---
+++ src/main/webapp/js/wordCloud.js
@@ -0,0 +1,46 @@
+var fill = d3.scale.category20();
+
+var width = 700;
+var height = 700;
+
+var userScale = d3.scale.linear().range([10,80]);
+//asdasdasdasdsadsd
+d3.json("./api/stats/userData.json", function(data){
+  if (data.length > 100){
+    var users = data.slice(0,100).map(function(data){return {text: data.name, size: data.messageCount}});
+  }else{
+    var users = data.map(function(data){return {text: data.name, size: data.messageCount}});
+  }
+  console.log(users);
+  userScale.domain([
+    d3.min(users, function(data){return data.size}),
+    d3.max(users, function(data){return data.size})
+  ]);
+  d3.layout.cloud()
+      .size([width, height])
+      .words(users)
+      .padding(0)
+      .font("Impact")
+      .fontSize(function(data) { return userScale(data.size); })
+      .on("end", draw)
+      .start();
+});
+
+function draw(words) {
+  d3.select("#wordCloud").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+    .selectAll("text")
+      .data(words)
+    .enter().append("text")
+      .style("font-size", function(d) { return d.size + "px"; })
+      .style("font-family", "Impact")
+      .style("fill", function(d, i) { return fill(i); })
+      .attr("text-anchor", "middle")
+      .attr("transform", function(d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function(d) { return d.text; });
+}
