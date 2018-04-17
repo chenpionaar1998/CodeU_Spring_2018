0cd75d4465c387a3e8713f3439281f035ea09e21
sherryyang2016 <sherryyang2016@gmail.com>
2018-04-16 21:58:53 -0700

    update for test_DoGet

--- src/test/java/codeu/controller/ProfileServletTest.java
+++ src/test/java/codeu/controller/ProfileServletTest.java
@@ -9,7 +9,9 @@ import codeu.model.store.basic.UserStore;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.UUID;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -43,7 +45,7 @@ public class ProfileServletTest {
 
     mockResponse = Mockito.mock(HttpServletResponse.class);
     mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
-    Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/chat.jsp"))
+    Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/profile.jsp"))
         .thenReturn(mockRequestDispatcher);
 
     mockMessageStore = Mockito.mock(MessageStore.class);
@@ -55,12 +57,33 @@ public class ProfileServletTest {
 
   @Test
   public void testDoGet() throws IOException, ServletException {
-    Mockito.when(mockRequest.getRequestURI()).thenReturn("/test_profile");
+    Mockito.when(mockRequest.getRequestURI()).thenReturn("/profile/test_profileUser");
+    
+    Queue<Message> testMessages = new LinkedList<Message>();
+    addSomeMessages(testMessages);
+    String testAbout = "This is just a sample About Me";
+    
+    UserStore mockUserStore = Mockito.mock(UserStore.class);
+    Mockito.when(mockUserStore.isUserRegistered("test_profileUser")).thenReturn(true);
+    
+    User mockUser = Mockito.mock(User.class);
+    Mockito.when(mockUserStore.getUser("test_profileUser")).thenReturn(mockUser);
+    Mockito.when(mockUser.getName()).thenReturn("test_profileUser");
+    Mockito.when(mockUser.getAbout()).thenReturn(testAbout);
+    Mockito.when(mockUser.getMessages()).thenReturn(testMessages);
     
     profileServlet.doGet(mockRequest, mockResponse);
-
+    
+    Mockito.verify(mockRequest).setAttribute("profileUser", mockUser.getName());
+    Mockito.verify(mockRequest).setAttribute("aboutUser", testAbout);
+    Mockito.verify(mockRequest).setAttribute("profileMessages", testMessages);
     Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
   }
   
+  private void addSomeMessages(Queue<Message> testMessages) {
+	  for (int i = 0; i < 16; i++) {
+	        testMessages.add(new Message(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID(),
+	        					"test message " + i, Instant.now()));
+	    }
+  }
 }
-
