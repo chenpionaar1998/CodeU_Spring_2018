69275061d7efacc3539808de2d59bea5796c70c2
sherryyang2016 <sherryyang2016@gmail.com>
2018-05-25 14:59:11 -0700

    fixing format in Image

--- src/main/java/codeu/model/data/Image.java
+++ src/main/java/codeu/model/data/Image.java
@@ -2,71 +2,67 @@ package codeu.model.data;
 
 import codeu.model.store.basic.ImageStore;
 
-import java.io.IOException;
 import java.net.*;
 import java.util.Scanner;
 import java.io.*;
 
 public class Image {
 	
-	private static final String TARGET_URL =
-            "https://vision.googleapis.com/v1/images:annotate?";
-	private static final String API_KEY =
-            "key=AIzaSyAmTbdJrzov7ZVwGBzCVHPTM8F1L913yZM";
-    private String url; 
-    private String [] labels;
+  private static final String TARGET_URL = "https://vision.googleapis.com/v1/images:annotate?";
+  private static final String API_KEY = "key=AIzaSyAmTbdJrzov7ZVwGBzCVHPTM8F1L913yZM";
+  private String url; 
 
-    public Image(String url) {
-    	this.url = url;
-    	getResponseFromAPI();
-    }
+  public Image(String url) {
+    this.url = url;
+    callToAPI();
+  }
     
-    public void getResponseFromAPI() {
-    	try {
-	    	URL serverUrl = new URL(TARGET_URL + API_KEY);
-	    	URLConnection urlConnection = serverUrl.openConnection();
-	    	HttpURLConnection httpConnection = (HttpURLConnection)urlConnection;
-	    	httpConnection.setRequestMethod("POST");
-	    	httpConnection.setRequestProperty("Content-Type", "application/json");
-	    	httpConnection.setDoOutput(true);
-	    	BufferedWriter httpRequestBodyWriter = new BufferedWriter(new
-	                OutputStreamWriter(httpConnection.getOutputStream()));
-	    	httpRequestBodyWriter.write
-	    	("{\"requests\":  [{ \"features\":  [ {\"type\": \"LABEL_DETECTION\""
-	    			+"}], \"image\": {\"source\": { \"ImageUri\":"
-	    			+" \"" + url + "\"}}}]}");
-	    	httpRequestBodyWriter.close();
-	    	String response = httpConnection.getResponseMessage(); 
-	    	
-	    	if (httpConnection.getInputStream() == null) {
-			   System.out.println("No stream");
-			   return;
-			} 
-			Scanner httpResponseScanner = new Scanner (httpConnection.getInputStream());
-			String resp = "";
-			while (httpResponseScanner.hasNext()) {
-			   String line = httpResponseScanner.nextLine();
-			   resp += line;
-			   System.out.println(line);  //  alternatively, print the line of response
-			}
-			httpResponseScanner.close();
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
-    }
+  public void callToAPI() {
+    try {
+      URL serverUrl = new URL(TARGET_URL + API_KEY);
+      URLConnection urlConnection = serverUrl.openConnection();
+      HttpURLConnection httpConnection = (HttpURLConnection)urlConnection;
+      httpConnection.setRequestMethod("POST");
+      httpConnection.setRequestProperty("Content-Type", "application/json");
+      httpConnection.setDoOutput(true);
+      BufferedWriter httpRequestBodyWriter = new BufferedWriter(new
+            OutputStreamWriter(httpConnection.getOutputStream()));
+      httpRequestBodyWriter.write
+      		("{\"requests\":  [{ \"features\":  [ {\"type\": \"LABEL_DETECTION\""
+			+"}], \"image\": {\"source\": { \"ImageUri\":"
+			+" \"" + url + "\"}}}]}");
+      httpRequestBodyWriter.close();
+      String response = httpConnection.getResponseMessage(); 
+	
+      if (httpConnection.getInputStream() == null) {
+	    System.out.println("No stream");
+	    return;
+      } 
+      Scanner httpResponseScanner = new Scanner (httpConnection.getInputStream());
+      String resp = "";
+      while (httpResponseScanner.hasNext()) {
+	    String line = httpResponseScanner.nextLine();
+	    resp += line;
+	    System.out.println(line);  //  alternatively, print the line of response
+      }
+      httpResponseScanner.close();
+	} catch(Exception e) {
+		System.out.println(e.getMessage());
+	}
+}
 
-    public String getUrl() {
-        return url;
-    }
+  public String getUrl() {
+    return url;
+  }
 
-    public String getHtml() {
-        return "<a href=" + url + "><img style=\"max-width:500px\" src=" + url + "></a> ";
-    }
+  public String getHtml() {
+   return "<a href=" + url + "><img style=\"max-width:500px\" src=" + url + "></a> ";
+  }
    
-    /* gets imagee from image store by this url. If image not in imagestore
-     * imagestore will add a image by this url */
-    public static Image getImageFromUrl(String url) {
-    	return ImageStore.getInstance().getImageFromUrl(url);
-    }
+  /* gets imagee from image store by this url. If image not in imagestore
+   * imagestore will add a image by this url */
+  public static Image getImageFromUrl(String url) {
+    return ImageStore.getInstance().getImageFromUrl(url);
+  }
 
 }
