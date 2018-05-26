7b7f861658c9c69e864778204f607930fc81007c
chenpionaar1998 <chenpionaar1998@gmail.com>
2018-05-25 17:07:25 -0700

    new feilds added for image class

--- src/main/java/codeu/model/data/Image.java
+++ src/main/java/codeu/model/data/Image.java
@@ -1,11 +1,14 @@
 package codeu.model.data;
 
 import codeu.model.store.basic.ImageStore;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Image {
-    
-    String url; 
+
+    String url;
     String [] labels;
+    Set<String> descriptions = new HashSet<>();
 
     public Image(String url) {
     	this.url = url;
@@ -18,11 +21,18 @@ public class Image {
     public String getHtml() {
         return "<a href=" + url + "><img style=\"max-width:500px\" src=" + url + "></a> ";
     }
-   
+
     /* gets imagee from image store by this url. If image not in imagestore
      * imagestore will add a image by this url */
     public static Image getImageFromUrl(String url) {
     	return ImageStore.getInstance().getImageFromUrl(url);
     }
 
+    public Set<String> getDescription() {
+      return descriptions;
+    }
+
+    public void addDiscription(String description){
+      descriptions.add(description);
+    }
 }
